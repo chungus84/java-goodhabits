fc75df9b145d1b9ad16032f653ccaa499222c51c
kenichi beveridge <kenbev84@gmail.com>
2023-12-31 16:59:01 +0000

    Added first User Model Test

--- backend/src/main/java/com/goodhabits/habitsbackend/models/User.java
+++ backend/src/main/java/com/goodhabits/habitsbackend/models/User.java
@@ -20,6 +20,34 @@ public class User {
     @NotEmpty(message =  "There must be a userName")
     private String userName;
 
+    public User(String _id, String userId, String userName) {
+        this._id = _id;
+        this.userId = userId;
+        this.userName = userName;
 
+    }
 
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
---
+++ backend/src/test/java/com/goodhabits/habitsbackend/model/UserModelTest.java
@@ -0,0 +1,42 @@
+package com.goodhabits.habitsbackend.model;
+
+import com.goodhabits.habitsbackend.models.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserModelTest {
+
+    User user;
+
+    String _id;
+    String userId;
+    String userName;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("Create a new user using the User model")
+    void userModel_whenGivenCorrectArguments_ShouldReturnAUserInstance() {
+
+       // Arrange
+        _id = "123ABC";
+        userId = "456DEF";
+        userName = "Test";
+
+        // Act
+        user = new User(_id, userId, userName);
+
+        // Assert
+        assertEquals(user.get_id(), _id, "Expected _id is different");
+        assertEquals(user.getUserId(), userId, "Expected userId is different");
+        assertEquals(user.getUserName(), userName, "Expected userName is different");
+    }
+
+
+}
