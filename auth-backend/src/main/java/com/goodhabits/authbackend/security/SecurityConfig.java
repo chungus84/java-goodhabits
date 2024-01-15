9521d323c36cd2ed049b523fe1c8d75f562306e4
kenichi beveridge <kenbev84@gmail.com>
2024-01-15 20:46:32 +0000

    set up Security Config

--- auth-backend/src/main/java/com/goodhabits/authbackend/security/SecurityConfig.java
+++ auth-backend/src/main/java/com/goodhabits/authbackend/security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.goodhabits.authbackend.security;
 
 
+import com.goodhabits.authbackend.security.filter.AuthenticationFilter;
 import com.goodhabits.authbackend.security.manager.CustomAuthenticationManager;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        Auth
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .addFilter(authenticationFilter)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
     }
 }
